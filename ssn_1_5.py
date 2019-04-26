ffb50ed670ce811281355a95c6db9e0146895a27
B Choi <bghechoi@gmail.com>
2019-04-26 12:53:30 +0200

    try to improve ssn

---
+++ ssn_1_5.py
@@ -0,0 +1,14 @@
+# for fast array manipulation
+import numpy as np
+# for plotting
+import matplotlib.pyplot as plt
+# for numerical ODE integration
+from scipy.integrate import odeint
+# for nonlinear equations
+from scipy.optimize import fsolve
+# to display plots in-line
+# %matplotlib inline
+# widget
+from IPython.display import clear_output
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import cnames
